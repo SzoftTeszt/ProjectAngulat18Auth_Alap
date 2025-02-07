676fe6e84e256731a38ce25e2f4a2d326155d4ce
AttilaJager <jagerattila@gmail.com>
2025-02-07 21:00:57 +0100

    AuthRepair

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SpidersComponent } from './spiders/spiders.component';
+import { SignUPComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { VerifyemailComponent } from './verifyemail/verifyemail.component';
+import { UsersComponent } from './users/users.component';
+import { adminGuard } from './admin.guard';
+
+import { ProfileComponent } from './profile/profile.component';
+import { loggedUserGuard } from './logged-user.guard';
+import { DataComponent } from './data/data.component';
+
+const routes: Routes = [
+  {path:"spiders", component:SpidersComponent},
+  {path:"signup", component:SignUPComponent},
+  {path:"signin", component:SignInComponent},
+  {path:"verifymail", component:VerifyemailComponent},
+  // {path:"users", component:UsersComponent, canActivate:[adminGuard]},
+  {path:"users", component:UsersComponent},
+  {path:"data", component:DataComponent},
+  {path:"profile", component:ProfileComponent, canActivate:[loggedUserGuard]},
+  {path:"", redirectTo:"signup", pathMatch:"full"},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
